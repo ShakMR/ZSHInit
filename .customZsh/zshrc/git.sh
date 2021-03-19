12c727a7a1cb87c40cc493c60ab30e48179350d9
ShakMR <borja.arias.upc@gmail.com>
2021-03-19 17:02:55 +0100

    Add more aliases

--- .customZsh/zshrc/git.sh
+++ .customZsh/zshrc/git.sh
@@ -1,6 +1,8 @@
 
 git config --global alias.comdiff 'log --graph --abbrev-commit --date=relative master..HEAD'
 git config --global alias.can 'commit --amend --no-edit'
+git config --global alias.p 'push origin HEAD'
+git config --global alias.pfoh 'git push --force-with-lease origin HEAD'
 
 openPR () {
     url=$(git remote show origin | grep "Push *URL" | cut -d "@" -f 2 | sed 's/:/\//' | sed -e 's/\.git//' | xargs -I % echo https://%/compare/$(git branch | grep \* | cut -d ' ' -f2)?expand=1)
