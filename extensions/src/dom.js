281ec126e740ebdeaacb63586239d24603103826
Borja Arias <borja.arias.upc@gmail.com>
2025-06-10 15:34:38 +0200

    Remove extenstion and function updates

--- extensions/src/dom.js
+++
@@ -1,7 +0,0 @@
-export const injectStyles = (styles) => {
-  document.head.innerHTML = document.head.innerHTML + styles;
-}
-
-export const addToDom = (element) => {
-  document.body.appendChild(element);
-}
